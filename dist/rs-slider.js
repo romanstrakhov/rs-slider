ad6a9003984ac1d1576861fd963dfb59cb2abde4
Roman Strakhov <roman@strakhov.ru>
2018-04-11 06:10:51 +0300

    basic layout & base event handling

--- dist/rs-slider.js
+++ dist/rs-slider.js
@@ -3,12 +3,78 @@ function Slider ( sliderID, sliderImages ) { // class constructor
 
   this.id = sliderID;
   this.images = sliderImages;
+  this.currentImage = 1;
+  this.numImages = sliderImages.length;
+
 
   let placeholder = document.getElementById(this.id);
+
+  this.imagesNode = placeholder.querySelectorAll('img.rsslider__image');
+
+
   console.log(placeholder);
   // console.info(arguments)
 
-  placeholder.innerHTML = `Script for ${this.id} is loaded.`;
+  // placeholder.innerHTML = `Script for ${this.id} is loaded.`;
+
+	placeholder.classList.add('rsslider');
+
+
+	var thisSlider = this;
+
+  placeholder.addEventListener('click', function(e){
+
+  	// console.log('click:' + this.id);
+
+  	if (e.target.classList.contains('rsslider__prev')) {
+  		console.log('goPrev');
+  		thisSlider.goPrev();
+  	}
+
+  	if (e.target.classList.contains('rsslider__next')) {
+  		console.log('goNext');
+  		thisSlider.goNext();
+  	}
+
+  })
+
+
+	Object.defineProperty( this, 'goNext', { 
+	  value: function () {
+
+			console.log('wentNext');
+	  	// exit 0;	
+
+	  },
+	  enumerable: true,
+	  configurable: true,
+	  writable: true
+	} );
+
+	Object.defineProperty( this, 'goPrev', { 
+	  value: function () {
+
+			console.log('wentPrev');
+	  	// exit 0;	
+
+	  },
+	  enumerable: true,
+	  configurable: true,
+	  writable: true
+	} );
+
+
+
+/*  this.goNext = function() {
+  	console.log('goNext');
+  	exit 0;	
+  };
+
+  this.goPrev = function() {
+  	console.log('goPrev');
+  	exit 0;	
+  };
+*/
 
 
 } 
