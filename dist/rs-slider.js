ab8851b2d7296802c33df13621d0246cff7eb4d1
Roman Strakhov <roman@strakhov.ru>
2018-04-12 02:18:39 +0300

    Release 1

--- dist/rs-slider.js
+++ dist/rs-slider.js
@@ -1,46 +1,103 @@
 'use strict';
 
+var DEBUG = true;
+
 function Slider(sliderID, sliderImages) {
   // class constructor
 
   this.id = sliderID;
   this.images = sliderImages;
-  this.currentImage = 1;
+
   this.numImages = sliderImages.length;
+  this.listImages = [];
+  this.currentImage = 1;
+
+  if (this.numImages <= 0) {
+    return false;
+  }
 
   var placeholder = document.getElementById(this.id);
 
-  this.imagesNode = placeholder.querySelectorAll('img.rsslider__image');
+  if (!placeholder) {
+    return false;
+  }
 
-  console.log(placeholder);
-  // console.info(arguments)
+  //  add arrows
 
-  // placeholder.innerHTML = `Script for ${this.id} is loaded.`;
+  this.arrowPrev = document.createElement('div');
+  this.arrowPrev.classList.add('rsslider__prev');
+  placeholder.appendChild(this.arrowPrev);
+
+  this.arrowNext = document.createElement('div');
+  this.arrowNext.classList.add('rsslider__next');
+  placeholder.appendChild(this.arrowNext);
+
+  // add images
+  sliderImages.forEach(function (e, i, a) {
+    var imageItem = document.createElement('img');
+    imageItem.setAttribute('src', e);
+    imageItem.classList.add('rsslider__image');
+    placeholder.appendChild(imageItem);
+    this.listImages.push(imageItem);
+  }, this);
+
+  Object.defineProperty(this, 'updateView', {
+    value: function value() {
+
+      if (this.currentImage < 1) {
+        this.currentImage = 1;
+      } else if (this.currentImage > this.numImages) {
+        this.currentImage = this.numImages;
+      }
+
+      this.arrowPrev.classList.remove('hidden');
+      this.arrowNext.classList.remove('hidden');
+
+      if (this.currentImage == 1) {
+        this.arrowPrev.classList.add('hidden');
+      }
+      if (this.currentImage == this.numImages) {
+        this.arrowNext.classList.add('hidden');
+      }
+
+      this.listImages.forEach(function (e, i, a) {
+        if (i + 1 !== this.currentImage) {
+          e.classList.add('hidden');
+        } else {
+          e.classList.remove('hidden');
+        }
+      }, this);
+
+      return this;
+    },
+    enumerable: true,
+    configurable: true,
+    writable: true
+  });
 
   placeholder.classList.add('rsslider');
 
-  var thisSlider = this;
+  this.updateView();
 
+  var thisSlider = this;
   placeholder.addEventListener('click', function (e) {
 
-    // console.log('click:' + this.id);
-
     if (e.target.classList.contains('rsslider__prev')) {
-      console.log('goPrev');
       thisSlider.goPrev();
     }
 
     if (e.target.classList.contains('rsslider__next')) {
-      console.log('goNext');
       thisSlider.goNext();
     }
   });
 
   Object.defineProperty(this, 'goNext', {
+
     value: function value() {
 
-      console.log('wentNext');
-      // exit 0;	
+      this.currentImage++;
+      this.updateView();
+      return this;
     },
     enumerable: true,
     configurable: true,
@@ -50,22 +107,21 @@ function Slider(sliderID, sliderImages) {
   Object.defineProperty(this, 'goPrev', {
     value: function value() {
 
-      console.log('wentPrev');
-      // exit 0;	
+      this.currentImage--;
+      this.updateView();
+      return this;
     },
     enumerable: true,
     configurable: true,
     writable: true
   });
+};
 
-  /*  this.goNext = function() {
-    	console.log('goNext');
-    	exit 0;	
-    };
-  
-    this.goPrev = function() {
-    	console.log('goPrev');
-    	exit 0;	
-    };
-  */
-}
+function debuglog(item) {
+  if (DEBUG) {
+    console.log(item);
+    return 1;
+  } else {
+    return 0;
+  }
+};
--- js/rs-slider.js
+++ js/rs-slider.js
@@ -1,80 +1,135 @@
 
+const DEBUG = true;
+
 function Slider ( sliderID, sliderImages ) { // class constructor
 
   this.id = sliderID;
   this.images = sliderImages;
-  this.currentImage = 1;
-  this.numImages = sliderImages.length;
 
+  this.numImages = sliderImages.length;
+  this.listImages = [];
+  this.currentImage = 1;
+
+  if (this.numImages<=0) {
+    return false;
+  }
 
   let placeholder = document.getElementById(this.id);
 
-  this.imagesNode = placeholder.querySelectorAll('img.rsslider__image');
+  if (!placeholder) {
+    return false;
+  }
+
+  //  add arrows
+
+  this.arrowPrev = document.createElement('div');
+  this.arrowPrev.classList.add('rsslider__prev');
+  placeholder.appendChild(this.arrowPrev);
+
+  this.arrowNext = document.createElement('div');
+  this.arrowNext.classList.add('rsslider__next');
+  placeholder.appendChild(this.arrowNext);
+
+  // add images
+  sliderImages.forEach( function(e, i, a) {
+    let imageItem = document.createElement('img');
+    imageItem.setAttribute('src', e);
+    imageItem.classList.add('rsslider__image');
+    placeholder.appendChild(imageItem);
+    this.listImages.push(imageItem);
+  }, this);
 
 
-  console.log(placeholder);
-  // console.info(arguments)
+  Object.defineProperty( this, 'updateView', { 
+    value: function () {
 
-  // placeholder.innerHTML = `Script for ${this.id} is loaded.`;
+    if (this.currentImage<1) {
+      this.currentImage = 1;
+    } else if (this.currentImage>this.numImages) {
+      this.currentImage = this.numImages;
+    }
 
-	placeholder.classList.add('rsslider');
+    this.arrowPrev.classList.remove('hidden');
+    this.arrowNext.classList.remove('hidden');
 
+    if (this.currentImage==1) {
+      this.arrowPrev.classList.add('hidden');
+    } 
+    if (this.currentImage==this.numImages) {
+      this.arrowNext.classList.add('hidden');
+    }
+    
+    this.listImages.forEach( function(e, i, a) {
+      if ( i + 1 !== this.currentImage ) {
+        e.classList.add('hidden');
+      } else {
+        e.classList.remove('hidden');
+      }
+    }, this);
 
-	var thisSlider = this;
+    return this;
 
+    },
+  enumerable: true,
+  configurable: true,
+  writable: true
+  });
+
+  
+  placeholder.classList.add('rsslider');
+
+  this.updateView();
+  
+  var thisSlider = this;
   placeholder.addEventListener('click', function(e){
 
-  	// console.log('click:' + this.id);
-
   	if (e.target.classList.contains('rsslider__prev')) {
-  		console.log('goPrev');
   		thisSlider.goPrev();
   	}
 
   	if (e.target.classList.contains('rsslider__next')) {
-  		console.log('goNext');
   		thisSlider.goNext();
   	}
 
-  })
+  });
 
 
-	Object.defineProperty( this, 'goNext', { 
-	  value: function () {
+  Object.defineProperty( this, 'goNext', { 
+    
+    value: function () {
 
-			console.log('wentNext');
-	  	// exit 0;	
+    this.currentImage++;
+    this.updateView();
+    return this;
 
-	  },
-	  enumerable: true,
-	  configurable: true,
-	  writable: true
-	} );
-
-	Object.defineProperty( this, 'goPrev', { 
-	  value: function () {
-
-			console.log('wentPrev');
-	  	// exit 0;	
-
-	  },
-	  enumerable: true,
-	  configurable: true,
-	  writable: true
-	} );
+    },
+  enumerable: true,
+  configurable: true,
+  writable: true
+  });
 
 
+  Object.defineProperty( this, 'goPrev', { 
+    value: function () {
 
-/*  this.goNext = function() {
-  	console.log('goNext');
-  	exit 0;	
-  };
+      this.currentImage--;
+      this.updateView();
+      return this;
 
-  this.goPrev = function() {
-  	console.log('goPrev');
-  	exit 0;	
-  };
-*/
+    },
+    enumerable: true,
+    configurable: true,
+    writable: true
+  });
+
+};
 
 
-} 
+function debuglog(item) {
+  if ( DEBUG ) {
+    console.log(item);
+    return 1;
+  } else {
+    return 0;
+  }
+};
